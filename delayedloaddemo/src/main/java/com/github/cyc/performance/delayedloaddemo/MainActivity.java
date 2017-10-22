f6b0b2522b5eae872ac5d573b8c80836ee0d7110
chongyucaiyan <chongyucaiyan@163.com>
2017-10-22 23:11:03 +0800

    Add delayed load demo

--- delayedloaddemo/src/main/java/com/github/cyc/performance/delayedloaddemo/MainActivity.java
+++ delayedloaddemo/src/main/java/com/github/cyc/performance/delayedloaddemo/MainActivity.java
@@ -1,13 +1,43 @@
 package com.github.cyc.performance.delayedloaddemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private boolean mLoadedView = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initContentView();
+    }
+
+    private void initContentView() {
+        findViewById(R.id.btn_load_view).setOnClickListener(this);
+    }
+
+    private void loadView() {
+        View vGreeting = ((ViewStub) findViewById(R.id.vs_greeting)).inflate();
+        // 或者
+//        findViewById(R.id.vs_greeting).setVisibility(View.VISIBLE);
+        mLoadedView = true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_load_view:
+                if (!mLoadedView) {
+                    // 加载视图
+                    loadView();
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
